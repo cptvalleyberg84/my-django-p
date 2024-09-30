5341cd6c11ca788ebb536651fa84aeb02e1c84b8
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-09-30 11:42:13 +0000

    Add heroku settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ogv%)_0oomicsrcrwi=*wgcjl8nkao+=e7)3cz3#i0ch(=sao'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-cptvalleyberg-mydjangop-zpgw39jbf1o.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-cptvalleyberg-mydjangop-zpgw39jbf1o.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
